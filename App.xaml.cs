83dddfa24fa34393d6db6573ea6aff3b61ed3e5f
aleksandrnedakin <aleksandrnedakin@0B7E>
2024-03-27 23:59:12 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IT2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
